616da7b89092d482abca9ec4e69d5f80c2f7351f
Vladislav Markov <markov.vladislav1998@gmail.com>
2020-03-24 23:00:58 +0300

    Create library project

---
+++ Composed/Composed.h
@@ -0,0 +1,19 @@
+//
+//  Composed.h
+//  Composed
+//
+//  Created by Vladislav Markov on 24.03.2020.
+//  Copyright © 2020 farllight. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Composed.
+FOUNDATION_EXPORT double ComposedVersionNumber;
+
+//! Project version string for Composed.
+FOUNDATION_EXPORT const unsigned char ComposedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Composed/PublicHeader.h>
+
+
